4f0acd6601c504b837ed21eb8bd2cfe0895e2849
fymorGod <mufinagamer@gmail.com>
2024-01-16 17:43:35 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/fymorGod/gopportunities/router"
+import (
+	"github.com/fymorGod/gopportunities/config"
+	"github.com/fymorGod/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	//Initialize Router
 	router.Initialize()
 }

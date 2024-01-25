71c8399a3074be0d247edd449c5f8cfe82586842
¨fymorGod¨ <fymor7@gmail.com>
2024-01-25 14:00:05 -0300

    feat: request to updated router

--- handlers/request.go
+++ handlers/request.go
@@ -37,3 +37,23 @@ func (r *CreateOpeningRequest) Validate() error {
 	}
 	return nil
 }
+
+// UpdateOpening
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	// if any field is provided, validation is truthy
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	// if none of the fields are provided, validation is
+	return fmt.Errorf("at least one valid field must be provided")
+}
